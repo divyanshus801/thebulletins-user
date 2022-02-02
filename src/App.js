38036bd69bbab9996b2e3ab55d6de9f01b7efd90
divyanshu <divyanshuis801@gmail.com>
2022-02-02 21:00:43 +0530

    added meta tag for main website

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/homePage";
 import NewsPage from "./pages/NewsPage/newsPage";
+import Image from "../public/images/thebulletins.png";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Helmet>
+          <meta property="og:title" content="European Travel Destinations" />
+          <meta property="og:type" content="latest news" />
+          <meta property="og:image" content={Image} />
+          <meta property="og:url" content="https://www.thebulletins.in/" />
+          <meta name="twitter:card" content="summary_large_image" />
+
+          <meta
+            property="og:description"
+            content="Latest News Headlines: read all news quickly | thebulletins."
+          />
+        </Helmet>
         <Routes>
           <Route path="/" exact element={<HomePage />} />
           <Route path="/fullNews/:newsId" exact element={<NewsPage />} />
